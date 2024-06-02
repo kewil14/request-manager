1c4bc4b683af861ffc0c4214c14ecc34fb2e09b0
Tene Romaric <tenrom142@gmail.com>
2024-06-02 13:49:40 +0100

    login

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
+import 'package:request_manager/environment/environment.dart';
 import 'package:request_manager/routes/name_route.dart';
+import 'package:http/http.dart' as http;
 
 class AuthController extends GetxController {
   final GlobalKey<FormState> formLoginEmailKey = GlobalKey<FormState>();
@@ -13,10 +17,34 @@ class AuthController extends GetxController {
 
   Future<void> login(BuildContext context) async {
     if (formLoginEmailKey.currentState!.validate()) {
-      isLoadLogin.value = true;
-      await Future.delayed(const Duration(seconds: 2));
-      context.pushNamed(AppRoutes.home);
-      isLoadLogin.value = false;
+      var response = await http.post(Uri.parse(baseUrl + '/api/users'),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({
+            "matricule": identifierController.text,
+            "password": passwordController.text,
+          }));
+
+      /*  if (response.statusCode == 200) {
+        // La requête a réussi
+        print('Requête POST réussie');
+        final responseData = json.decode(response.body);
+        */ /*final username = responseData['username'];
+        final email = responseData['email'];
+        final numberPhone = responseData['numberPhone'];*/ /*
+        final datas = responseData['body'];
+
+        */ /* SharedPreferences prefs = await SharedPreferences.getInstance();
+       await prefs.setString('username', username);
+        await prefs.setString('email', email);
+        await prefs.setString('numberPhone', numberPhone);*/ /*
+        return datas;
+      } else if (response.statusCode == 401) {
+        const res = 'revfrty';
+      }*/
     }
+    isLoadLogin.value = true;
+    await Future.delayed(const Duration(seconds: 2));
+    context.pushNamed(AppRoutes.home);
+    isLoadLogin.value = false;
   }
 }
